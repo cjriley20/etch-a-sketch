b06ca6916dce0e6aeb1570ce95f37ad72fe7cf11
Chris Riley <20461606+cjriley20@users.noreply.github.com>
2024-03-05 22:19:29 -0500

    Test getting equal rows

--- script.js
+++ script.js
@@ -1,16 +1,12 @@
 const container = document.querySelector('div.container');
 
-const containerStyles = window.getComputedStyle(container);
-const containerWidth = containerStyles.getPropertyValue('width');
-console.log(`Container width = ${containerWidth}`);
-
 const rows = 4;
 const cols = rows;
 
 for (let row = 0; row < rows; row++) {
-    for (let col = 0; col < cols; col++) {
-        const square = document.createElement('div');
-
-        container.appendChild(square);
-    }
+//     for (let col = 0; col < cols; col++) {
+        const row = document.createElement('div');
+        row.classList.add('row');
+        container.appendChild(row);
+//     }
 }
