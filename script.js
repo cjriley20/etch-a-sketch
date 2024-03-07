4abd5719d9fa14d95a39da3fb85179cc7af1d158
cjriley20 <20461606+cjriley20@users.noreply.github.com>
2024-03-06 22:02:38 -0500

    Add reset button to change grid

--- script.js
+++ script.js
@@ -1,15 +1,33 @@
-const grid = document.querySelector('div.grid');
+function initializeGrid(rows, cols) {
+  const grid = document.querySelector('div.grid');
 
-const rows = 4;
-const cols = rows;
-
-for (let i = 0; i < rows; i++) {
-  const row = document.createElement('div');
-  row.classList.add('row');
-  for (let j = 0; j < cols; j++) {
-    const col = document.createElement('div');
-    col.classList.add('column');
-    row.appendChild(col);
+  // Clear grid first.
+  while (grid.firstChild) {
+    grid.removeChild(grid.firstChild);
   }
-  grid.appendChild(row);
+
+  for (let i = 0; i < rows; i++) {
+    const row = document.createElement('div');
+    row.classList.add('row');
+    for (let j = 0; j < cols; j++) {
+      const col = document.createElement('div');
+      col.classList.add('column');
+      row.appendChild(col);
+    }
+    grid.appendChild(row);
+  }
 }
+
+const resetButton = document.querySelector(`#reset`);
+
+resetButton.addEventListener('click', (e) => {
+  const inputString = prompt('Enter number of squares per side (< 100):', '16');
+
+  let rows = parseInt(inputString);
+  rows = Math.min(rows, 100);
+  rows = Math.max(rows, 1);
+
+  initializeGrid(rows, rows);
+})
+
+initializeGrid(8, 8);
