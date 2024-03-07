68a62c977888f148f6902b0432c2090edbc37289
cjriley20 <20461606+cjriley20@users.noreply.github.com>
2024-03-06 21:41:58 -0500

    Add rest button and fix grid

--- script.js
+++ script.js
@@ -1,12 +1,15 @@
-const container = document.querySelector('div.container');
+const grid = document.querySelector('div.grid');
 
 const rows = 4;
 const cols = rows;
 
-for (let row = 0; row < rows; row++) {
-//     for (let col = 0; col < cols; col++) {
-        const row = document.createElement('div');
-        row.classList.add('row');
-        container.appendChild(row);
-//     }
+for (let i = 0; i < rows; i++) {
+  const row = document.createElement('div');
+  row.classList.add('row');
+  for (let j = 0; j < cols; j++) {
+    const col = document.createElement('div');
+    col.classList.add('column');
+    row.appendChild(col);
+  }
+  grid.appendChild(row);
 }
